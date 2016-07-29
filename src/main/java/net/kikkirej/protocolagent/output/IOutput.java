51051ed005a82c1e7bd08ef8009c7478534e3157
Fabian Lemke <fabian.lemke@online.de>
2016-07-29 15:45:43 +0200

    Alles einmal gelöscht

--- src/main/java/net/kikkirej/protocolagent/output/IOutput.java
+++
@@ -1,7 +0,0 @@
-package net.kikkirej.protocolagent.output;
-
-public interface IOutput {
-	void write(String line);
-	
-	void writeLine(String line);
-}
--- src/main/java/net/kikkirej/protocolagent/properties/PropertyKeys.java
+++
@@ -1,9 +0,0 @@
-package net.kikkirej.protocolagent.properties;
-/**
- * Keys for the possible Properties.
- * @author Kikkirej
- *
- */
-public class PropertyKeys {
-
-}

1b728953778c605caa58977633fba0ba632cd709
Fabian Lemke <info@kikkirej.net>
2016-08-03 20:49:58 +0200

    not needed imports deleted

--- src/main/java/net/kikkirej/protocolagent/flags/flagactions/ConfFlagAction.java
+++ src/main/java/net/kikkirej/protocolagent/flags/flagactions/ConfFlagAction.java
@@ -1,8 +1,6 @@
-package net.kikkirej.protocolagent.flags.flagactions;
+	package net.kikkirej.protocolagent.flags.flagactions;
 
 import net.kikkirej.protocolagent.flags.Flag;
-import net.kikkirej.protocolagent.options.screen.OptionsScreen;
-import net.kikkirej.protocolagent.properties.PropertyKeys;
 import net.kikkirej.protocolagent.properties.PropertyManager;
 
 public class ConfFlagAction implements IFlagAction {
--- src/main/java/net/kikkirej/protocolagent/properties/PropertyFile.java
+++ src/main/java/net/kikkirej/protocolagent/properties/PropertyFile.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-import java.net.URL;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
